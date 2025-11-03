2461a4c11a9d40fa619e8f86b78b7fe6f522d861
Ali Taheri <aladdin003418@gmail.com>
2025-11-03 03:52:39 +0000

    در منوی همبرگری برای جمع شدن منو دو دکمه روی یکدیگر وجود دارند و رابط کا

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -206,10 +206,6 @@ const Sidebar = React.forwardRef<
             side={side}
           >
             <SheetTitle className="sr-only">منوی اصلی</SheetTitle>
-             <SheetClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-                <X className="h-6 w-6" />
-                <span className="sr-only">Close</span>
-            </SheetClose>
             <div className="flex h-full w-full flex-col pt-8">{children}</div>
           </SheetContent>
         </Sheet>
